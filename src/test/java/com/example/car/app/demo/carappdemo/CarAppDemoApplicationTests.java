dda90d649778eff09a399525feb06bebbb5d27fb
Sanjaya Medonsa <sanjayamedonsa@Sanjayas-Air.lan>
2021-12-05 08:57:08 +0000

    Added initial skeletal project for the car app demo

---
+++ src/test/java/com/example/car/app/demo/carappdemo/CarAppDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.car.app.demo.carappdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarAppDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
